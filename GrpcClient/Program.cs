cbee01abe52a4674cd7de969103a4deb2686f419
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2020-03-10 00:21:23 -0300

    Adds sample client & server projects of gRPC.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService1;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var greetClient = new Greeter.GreeterClient(channel);
+            var hello = new HelloRequest();
+            hello.Name = "Matheus Veiga";
+            var response = await greetClient.SayHelloAsync(hello);
+            WriteLine($"gRPC Service Reply: {response.Message}");
+            ReadLine();
+
+            var helloStream = new HelloRequest();
+            using (var call = greetClient.SayHelloByStream(helloStream))
+            {
+                while(await call.ResponseStream.MoveNext())
+                {
+                    var currentMessage = call.ResponseStream.Current;
+                    WriteLine($"gRPC Service Reply: {currentMessage.Message}");
+                }                
+            }            
+            ReadLine();
+        }
+    }
+}
---
+++ GrpcService/Services/GreeterService.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+
+        public async override Task SayHelloByStream(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            var texts = new []
+            {
+                "Hello", "Matheus", "Veiga", "My", "Friend."
+            };
+
+            foreach (var text in texts)
+            {
+                await Task.Delay(1000);
+                var message = new HelloReply { Message = text };
+                await responseStream.WriteAsync(message);
+            }
+        }
+    }
+}
